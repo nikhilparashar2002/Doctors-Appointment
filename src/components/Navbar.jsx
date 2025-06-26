abbbea35606843a96ddfc6c18d772296334ef949
nikhilparashar2002 <nikhilsharma22092002@gmail.com>
2025-06-26 11:39:19 +0530

    fix css of menu and profile icon

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -31,7 +31,8 @@ function Navbar() {
           <hr className='border-none outline-none h-0.5 w-3/5 m-auto hidden ' />
         </NavLink>
       </ul>
-      <div>
+      <div className="flex items-center gap-3">
+        {/* Profile image and dropdown */}
         {
           token ?
             <div className='flex items-center gap-2 cursor-pointer group relative'>
@@ -41,7 +42,8 @@ function Navbar() {
               <img
                 className='w-2.5 '
                 src={assets.dropdown_icon} />
-              <div className='absolute top-0 right-0 pt-14 text-base font-medium text-gray-600 z-20 hidden group-hover:block '>
+              {/* Dropdown menu */}
+              <div className='absolute top-0 right-0 pt-14 text-base font-medium text-gray-600 z-20 hidden group-hover:block'>
                 <div className='min-w-48 bg-stone-100 rounded flex flex-col gap-4 p-4'>
                   <p
                     className='hover:text-black cursor-pointer'
@@ -65,21 +67,22 @@ function Navbar() {
               Create Account
             </button>
         }
-         <img onClick={()=>setShowMenu(true)} className='w-6 md:hidden' src={assets.menu_icon}/>
-         {/* Mobile menu */}
-         <div className={`${showMenu ? 'fixed w-full' : 'h-0 w-0'} md:hidden right-0 top-0 bottom-0 z-20 overflow-hidden bg-white transition-all`}>
-          <div className='flex items-center justify-between px-5 py-6'>
-            <img className='w-36' src={assets.logo} alt=''/>
-            <img className='w-7' src={assets.cross_icon} onClick={()=>setShowMenu(false)}/>
-          </div>
-          <ul className='flex flex-col items-center gap-2 mt-5 px-5 text-lg font-medium'> 
-            <NavLink onClick={()=>setShowMenu(false)} to='/' className=''><p>Home</p></NavLink>
-            <NavLink onClick={()=>setShowMenu(false)} to='/Doctor' className=''><p>All DOCTORS</p></NavLink>
-            <NavLink onClick={()=>setShowMenu(false)} to='/About' className=''><p>All DOCTORS</p></NavLink>
-            <NavLink onClick={()=>setShowMenu(false)} to='/Contact' className=''><p>CONTACT</p></NavLink>
-          </ul>
-         </div>
+        {/* Hamburger menu icon */}
+        <img onClick={() => setShowMenu(true)} className='w-6 md:hidden' src={assets.menu_icon} />
       </div>
+      {/* Mobile menu */}
+      <div className={`${showMenu ? 'fixed w-full' : 'h-0 w-0'} md:hidden right-0 top-0 bottom-0 z-20 overflow-hidden bg-white transition-all`}>
+        <div className='flex items-center justify-between px-5 py-6'>
+          <img className='w-36' src={assets.logo} alt=''/>
+          <img className='w-7' src={assets.cross_icon} onClick={()=>setShowMenu(false)}/>
+        </div>
+        <ul className='flex flex-col items-center gap-2 mt-5 px-5 text-lg font-medium'> 
+          <NavLink onClick={()=>setShowMenu(false)} to='/' className=''><p>Home</p></NavLink>
+          <NavLink onClick={()=>setShowMenu(false)} to='/Doctor' className=''><p>All DOCTORS</p></NavLink>
+          <NavLink onClick={()=>setShowMenu(false)} to='/About' className=''><p>All DOCTORS</p></NavLink>
+          <NavLink onClick={()=>setShowMenu(false)} to='/Contact' className=''><p>CONTACT</p></NavLink>
+        </ul>
+       </div>
     </div>
   )
 }
